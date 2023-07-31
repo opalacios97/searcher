b4c94339e8fbccd3e4d74515f3a296e1d8590e6c
Omar Palacios <opalacios@APRACOM.COM>
2023-07-31 02:48:15 -0500

    avance1 proyecto

---
+++ assets/js/searcher.js
@@ -0,0 +1,141 @@
+
+
+let loadProducts = () => {
+
+    let URL = `https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.json`
+
+    let URL2 = 'https://raw.githubusercontent.com/Bootcamp-Espol/Datos/main/products.xml'
+
+    let jsonData = [];
+    let xmlData = [];
+
+    // REQUEST 1
+    let request = ( ) => {
+
+        fetch( URL )
+            .then(response => response.json() ) 
+            .then(data1 => {
+
+            /* Callback por éxito: Procese el result */
+            //productos JSON
+
+            jsonData = data1
+
+            mostrar();
+
+        })
+            .catch(error => {
+            /* Callback por fallo: Procese el error */
+            console.log( error );
+        })
+        
+    
+    }
+     
+    // REQUEST 2
+    let request2 = ( ) => {
+
+        fetch( URL2 )
+        .then(response => response.text() ) /* Convierte el response a texto */
+        .then(result => {
+
+            let xml = (new DOMParser()).parseFromString(result, 'application/xml');
+
+            /* Callback por éxito: Procese el xml */
+            let names = xml.getElementsByTagName("name");
+            let srcs = xml.getElementsByTagName("src");     
+            let types = xml.getElementsByTagName("type");         
+            let prices = xml.getElementsByTagName("price");
+            
+            //productos XML
+            for (var i=0; i < names.length; i++){
+                xmlData.push({name: names[i].innerHTML, 
+                            src: srcs[i].innerHTML,
+                            type: types[i].innerHTML,
+                            price: prices[i].innerHTML
+                        });
+            }
+            
+            mostrar();
+              
+        })
+        .catch(error => {
+            
+            /* Callback por fallo: Procese el error */
+
+            console.log( error );
+
+        });
+        
+    }
+
+    let mostrar = () => {
+        let allData = jsonData.concat(xmlData);
+        
+        //mensaje a enviar
+        let mensajeEnviar = ""
+
+        allData.map(e =>{
+                mensajeEnviar = mensajeEnviar + `
+                <div class="col-xl-3 col-md-6 mb-xl-0 mb-4 mt-4">
+                  <div class="card card-blog card-plain">
+                    <div class="card-header p-0 mt-n4 mx-3">
+                      <a class="d-block shadow-xl border-radius-xl">
+                        <img src="${e.src}" alt="${e.name}" class="img-fluid shadow border-radius-xl">
+                      </a>
+                    </div>
+                    <div class="card-body p-3">
+                      <p class="mb-0 text-sm">${e.type}</p>
+                      <a href="javascript:;">
+                        <h5>
+                          ${e.name}
+                        </h5>
+                      </a>
+                      <p class="mb-4 text-sm">
+                        <b>Price: </b> $ ${e.price}
+                      </p>
+                    </div>
+                  </div>
+                </div>`;
+        })
+        //mensaje a cambiar
+        let mensajeCambiar = document.getElementsByClassName("row");
+
+        mensajeCambiar[0].innerHTML = mensajeEnviar;
+    }
+    request();
+    request2();
+
+
+
+
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    //Código a ejecutar
+
+    loadProducts();
+ 
+    // let element4 = document.getElementById("dropdownMenuButton");
+
+    // element4.addEventListener('change', (event) => {
+    //     //Código a ejecutar
+    //     //El event contiene la información del elemento seleccionado
+    //     let selectedValue = event.target.value 
+    //     loadDayForecastData(weather_data,selectedValue)
+        
+    //     let element = document.getElementById("loadinfo");
+        
+    //     element.addEventListener('click', (event) => {
+    //         //Código a ejecutar
+    //         loadWeekForecastData(weather_data,selectedValue);
+    //     });
+    // });
+
+    
+});
